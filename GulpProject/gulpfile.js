5b0aef58fc74f0c61f27b90a2b425374826bc6d8
Sergeykoval <skoval@s-pro.io>
2018-10-28 17:05:37 +0200

    add alexis

--- GulpProject/gulpfile.js
+++ GulpProject/gulpfile.js
@@ -2,15 +2,40 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 
 //CSS
+const autoprefixer = require('gulp-autoprefixer');
 const gulpminifycss = require('gulp-minify-css');
 const rename = require('gulp-rename');
+const plumber = require('gulp-plumber');
+const notify = require('gulp-notify');
 
 
+const browserSync = require('browser-sync');
+const reload = browserSync.reload;
+
+
+const config = {
+  server: {
+    baseDir: './',
+  },
+  host: 'localhost',
+  port: 9000,
+};
+
+gulp.task('browserSync', function() {
+  browserSync(config);
+});
 
 gulp.task('sass', function() {
  gulp.src('./sass/main.scss')
+ .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
+ .pipe(autoprefixer({
+    browsers: ['last 2 versions'],
+    cascade: false
+  }))
  .pipe(sass())
- .pipe(gulp.dest('./build/css/'));
+ .pipe(plumber.stop())
+ .pipe(gulp.dest('./build/css/'))
+ .pipe(reload({stream: true}));
 });
 
 gulp.task('css', function() {
@@ -26,7 +51,4 @@ gulp.task('watch', function(){
 
 gulp.task('build', ['sass','css']);
 
-
-
-
-
+gulp.task('default', ['build', 'browserSync', 'watch']);
