ec674b04d1535c0103ff22472d60da01f25b0303
Sergeykoval <skoval@s-pro.io>
2019-01-16 21:23:45 +0200

    add props

--- App/app/src/App.js
+++ App/app/src/App.js
@@ -4,10 +4,35 @@ import './App.css';
 import Header from './components/Header';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  handleClick = (name) => {
+    console.log('123', name);
+  }
+
   render() {
+    console.log('RENDER', this);
+    const links = [
+      {
+        title: 'Nav 1',
+        active: true,
+      },
+      {
+        title: 'Nav 2'
+      },
+    ];
     return (
       <div className="App">
-        <Header />
+        <Header
+          title="Header"
+          links={links}
+          handleClick={this.handleClick}
+        />
+        <Header
+          title="Header2"
+        />
         
         {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
--- App/app/src/components/Header/index.js
+++ App/app/src/components/Header/index.js
@@ -1,9 +1,26 @@
 import React from 'react';
 
-export default () => {
+export default (props) => {
+  console.log('HEADER PROPS', props);
   return (
-     <header className="App-header">
-      HEADER
+     <header className="App-header" style={{marginBottom: '20px'}}>
+      {props.title}
+      <div>
+        {
+          props.links && props.links.length > 0 &&
+          props.links.map((item, index) => {
+            return (
+              <button
+                key={index}
+                onClick={() => props.handleClick(item.title)}
+                className={item.active ? 'active' : ''}
+              >
+                {item.title}
+              </button>
+            )
+          })
+        }
+      </div>
     </header>
   )
 };
